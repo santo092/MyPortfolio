27d136e1aca93782f243d67f2d875fe25937c851
Alexis Villalpando <alexisvillalpando@trixiealexismbp.lan>
2019-03-02 21:33:57 -0800

    link Trivia game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,3 +3,5 @@ $(document).ready(function () {
 
 $("#crystal").attr("href", "https://santo092.github.io/unit-4-game/");
 });
+
+$("#crystal1").attr("href", "https://santo092.github.io/unit-4-game/");
