9ce7f94c272ccbcd8e5b843117048c866bad7f66
Alexis Villalpando <alexisvillalpando@Tsmacbookpro.local>
2019-03-15 01:24:03 -0700

    added styling with updated links

--- assets/javascript/game.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-
-
-$("#crystal").attr("href", "https://santo092.github.io/unit-4-game/");
-});
-
-$("#crystal1").attr("href", "https://santo092.github.io/unit-4-game/");
